7e6d72dcd57b7d90d2dce779df88c934846c1dc1
nabila <nabilatajrin@gmail.com>
2020-01-08 15:47:16 +0600

    03:46PM 08JAN2020 update

--- src/blog_django/views.py
+++ src/blog_django/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home_page(request):
-    return HttpResponse("<h1>Hello World</h1>")
+    return render(request,"hello_world.html")
+
+def about_page(request):
+    return HttpResponse("<h1>About Us</h1>")
+
+def contact_page(request):
+    return HttpResponse("<h1>Contact Us</h1>")
