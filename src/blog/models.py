df9d2f6e662d3a87c3e1508bb48172c7f3b02af2
nabila <nabilatajrin@gmail.com>
2020-01-09 09:45:21 +0600

    09:46AM 09JAN2020 update

---
+++ src/blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class BlogPost(models.Model):
+    title = models.TextField()
+    content = models.TextField(null=True, blank=True)
+
+
+class Blog:
+    title = 'Hello world'
+    content = 'something'
+
+
+
