9293626d458bd31ca6993389f430a8c3035cb712
nabila <nabilatajrin@gmail.com>
2020-01-09 07:26:42 +0600

    07:26AM 09JAN2020 update

--- src/blog_django/urls.py
+++ src/blog_django/urls.py
@@ -19,13 +19,15 @@ from django.urls import path, re_path
 from .views import (
     home_page,
     about_page,
-    contact_page
+    contact_page,
+    example_page
 )
 
 urlpatterns = [
     path('', home_page),
     re_path(r'^pages?/$', about_page),
     re_path(r'^about/$', about_page),
+    path('example/', example_page),
     path('contact/', contact_page),
     path('nabila-admin/', admin.site.urls),
 ]
--- src/blog_django/views.py
+++ src/blog_django/views.py
@@ -1,11 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.loader import get_template
+
 
 def home_page(request):
-    return render(request,"hello_world.html")
+    my_title = "Hello there..."
+    context = {"title"}
+    if request.user.is_authenticated:
+        context = {"title": my_title, "my_list": [1, 2, 3, 4, 5]}
+    return render(request, "home.html", context)
 
 def about_page(request):
-    return HttpResponse("<h1>About Us</h1>")
+    return render(request, "about.html", {"title": "About"})
 
 def contact_page(request):
-    return HttpResponse("<h1>Contact Us</h1>")
+    return render(request, "hello_world.html", {"title": "Contact us"})
+
+
+def example_page(request):
+    context         = {"title": "Example"}
+    template_name   = "hello_world.html"
+    template_obj    = get_template(template_name)
+    rendered_item   = template_obj.render(context)
+    return HttpResponse(rendered_item)
